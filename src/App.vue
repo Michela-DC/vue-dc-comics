bd207e8bb42bb721c1d6222e8adf8090cf390a22
Michela-DC <micheladelconte@outlook.com>
2022-03-08 17:15:26 +0100

    Added header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderDC/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HeaderDC from './components/HeaderDC.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderDC
   }
 }
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'Roboto', sans-serif;
+}
+
+ul li{
+  list-style: none;
+}
+
+a{
+  text-decoration: none;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 </style>
---
+++ src/components/HeaderDC.vue
@@ -0,0 +1,75 @@
+<template>
+  <header>
+      <div class="header-container">
+        <figure class="logo-wrapper">
+            <img class="logo" src="../assets/img/dc-logo.png" alt="">
+        </figure>
+
+        <ul class="navbar">
+            <li>
+                <a href="">charactres</a>
+            </li>
+            <li>
+                <a href="">comics</a>
+            </li>
+            <li>
+                <a href="">movies</a>
+            </li>
+            <li>
+                <a href="">tv</a>
+            </li>
+            <li>
+                <a href="">games</a>
+            </li>
+            <li>
+                <a href="">collectibles</a>
+            </li>
+            <li>
+                <a href="">videos</a>
+            </li>
+            <li>
+                <a href="">fans</a>
+            </li>
+            <li>
+                <a href="">news</a>
+            </li>
+            <li>
+                <a href="">shop</a>
+            </li> 
+        </ul>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderDC',
+}
+</script>
+
+<style scoped lang="scss">
+
+.header-container{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    height: 115px;
+}
+
+.logo-wrapper {
+    .logo{
+        height: 80px;
+    }
+}
+
+.navbar {
+    display: flex;
+    gap: 30px;
+    text-transform: uppercase;
+
+    a{
+        color: black;  
+        font-weight: bold;  
+    }
+}
+</style>
